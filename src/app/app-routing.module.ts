31abbac817f17bcdd370c0eeb443c70c029341ce
Cameron McGuffie <cameron@mcguffie.com.au>
2020-11-21 20:45:54 +1000

    Add homepage.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from "@angular/common";
 
+import { HomeComponent } from './home/home.component';
 import { SubjectListComponent } from './subject-list/subject-list.component';
 import { QuestionListComponent } from './question-list/question-list.component';
 
 const APP_COMPONENTS: any[] = [
+  HomeComponent,
   SubjectListComponent,
   QuestionListComponent
 ];
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'subjects', component: SubjectListComponent },
   { path: 'questions/:id', component: QuestionListComponent },
 ];
